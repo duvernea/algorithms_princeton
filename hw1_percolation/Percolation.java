d13c23a4e0e5b4b985acfa9a52b5e8bdfc0f2087
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-12-27 17:29:57 -0600

    hw1 - feat: Implement a basic constructor and main methods

--- hw1_percolation/Percolation.java
+++ hw1_percolation/Percolation.java
@@ -1,11 +1,26 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
 
    private int N;
+   private int[] sites;
     
    // create n-by-n grid, with all sites blocked 
    public Percolation(int n) {
       N = n;
+      sites = new int[N*N];
+      for (int i=0; i<N*N-1; i++) {
+         sites[i] = i;
+      }
    }  
+   public int getN() {
+      return N*N;
+   }
+   public int getValue(int idx) {
+      return sites[idx];
+   }
    // open site (row, col) if it is not open already
    public void open(int row, int col) {
 
@@ -28,6 +43,11 @@ public class Percolation {
    }
    // test client (optional)    
    public static void main(String[] args) {
-      System.out.println("Hello World"); 
+      Percolation myPercolation = new Percolation(5);
+      int n = myPercolation.getN();
+      System.out.println("Total Sites: " + n); 
+      for (int i=0; i<n-1; i++) {
+         System.out.println("Site " + i + " Value: " + myPercolation.getValue(i) );
+      }
    }
 }
