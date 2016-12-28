245e9d19f6e516a1f5e6e2dd0b27c759d6c22bb5
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-12-27 21:29:35 -0600

    hw1 - feat: Implement numberOfOpenSites() method

--- hw1_percolation/Percolation.java
+++ hw1_percolation/Percolation.java
@@ -7,6 +7,7 @@ public class Percolation {
    private int N;
    private int[] sites;
    private WeightedQuickUnionUF quickUnionUF;
+   private int openSites = 0;
     
    // create n-by-n grid, with all sites blocked 
    public Percolation(int n) {
@@ -17,39 +18,44 @@ public class Percolation {
       // }
       quickUnionUF = new WeightedQuickUnionUF(N*N);
    }  
-   public int getN() {
+   private int getN() {
       return N;
    }
-   public int getValue(int idx) {
+   private int getValue(int idx) {
       return sites[idx];
    }
    // open site (row, col) if it is not open already
    public void open(int row, int col) {
       if (xyValid(row, col)) {
          int index = xyTo1D(row, col);
-         sites[index] = 1;
-         //right
-         if (col != N) {
-            if (sites[index+1] == 1) {
-               quickUnionUF.union(index, index+1);
+         if (sites[index] == 0) {
+            openSites += 1;
+            sites[index] = 1;
+            System.out.println("Total Sites opened: " + openSites); 
+
+            //right
+            if (col != N) {
+               if (sites[index+1] == 1) {
+                  quickUnionUF.union(index, index+1);
+               }
             }
-         }
-         // left
-         if (col != 1) {
-            if (sites[index-1] == 1) {
-               quickUnionUF.union(index, index-1);
+            // left
+            if (col != 1) {
+               if (sites[index-1] == 1) {
+                  quickUnionUF.union(index, index-1);
+               }
             }
-         }
-         // up
-         if (row != 1) {
-            if (sites[index-N] == 1) {
-               quickUnionUF.union(index, index-N);
+            // up
+            if (row != 1) {
+               if (sites[index-N] == 1) {
+                  quickUnionUF.union(index, index-N);
+               }
             }
-         }
-         // down
-         if (row != N) {
-            if (sites[index+N] == 1) {
-               quickUnionUF.union(index, index+N);
+            // down
+            if (row != N) {
+               if (sites[index+N] == 1) {
+                  quickUnionUF.union(index, index+N);
+               }
             }
          }
       }
@@ -65,7 +71,7 @@ public class Percolation {
    }
    // number of open sites
    public int numberOfOpenSites() {
-      return 0;
+      return openSites;
    }
    // does the system percolate?    
    public boolean percolates()  {
@@ -80,6 +86,8 @@ public class Percolation {
       myPercolation.open(3,4);
       System.out.println("Opening 3,5");
       myPercolation.open(3,5);
+      System.out.println("Opening 7,7");
+      myPercolation.open(7,7);
       System.out.println("Is 3,4 open?: " + myPercolation.isOpen(3,4));
       System.out.println("Is 4,4 open?: " + myPercolation.isOpen(4,4));
       int index1 = myPercolation.xyTo1D(3,4);
@@ -87,7 +95,7 @@ public class Percolation {
       int index3 = myPercolation.xyTo1D(3,5);
       System.out.println("3,4, and 3,6 connected?: " + myPercolation.quickUnionUF.connected(index1, index2));
       System.out.println("3,4, and 3,5 connected?: " + myPercolation.quickUnionUF.connected(index1, index3));
-
+      System.out.println("Number of open sites: " + myPercolation.numberOfOpenSites());
       
    }
    public int xyTo1D(int row, int col) {
