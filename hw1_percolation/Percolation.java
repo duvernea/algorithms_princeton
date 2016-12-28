2e5f3eff1456675ac4ea25cd2751705eb55fbe20
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-12-27 22:29:28 -0600

    hw1 - feat: Add throw of exception in constructor

--- hw1_percolation/Percolation.java
+++ hw1_percolation/Percolation.java
@@ -10,13 +10,22 @@ public class Percolation {
    private int openSites = 0;
     
    // create n-by-n grid, with all sites blocked 
-   public Percolation(int n) {
-      N = n;
-      sites = new int[N*N];  // values initialized to 0 in java, none open
-      // for (int i=0; i<N*N-1; i++) {
-      //    sites[i] = i;
-      // }
-      quickUnionUF = new WeightedQuickUnionUF(N*N);
+   public Percolation(int n) throws java.lang.IllegalArgumentException {
+      if (n <= 0) {
+         throw new java.lang.IllegalArgumentException("n<=0");
+      } else {
+         N = n;
+         sites = new int[N*N];  // values initialized to 0 in java, none open
+         // for (int i=0; i<N*N-1; i++) {
+         //    sites[i] = i;
+         // }
+         quickUnionUF = new WeightedQuickUnionUF(N*N);
+         // union
+         // for (int i = 0; i <= N; i++) {
+         //    quickUnionUF.union(0, i);
+         //    quickUnionUF.union(N*N-1, N*N-1-i);
+         // }
+      }
    }  
    private int getN() {
       return N;
@@ -67,7 +76,16 @@ public class Percolation {
    } 
    // is site (row, col) full?
    public boolean isFull(int row, int col) {
-      return true;
+      boolean full = false;
+      if (isOpen(row, col)) {
+         int index = xyTo1D(row, col);
+         // need to add the virtual open site above top and below last rows
+         // for now try a hard code of '0', first row, first column
+         full = quickUnionUF.connected(index, 0);
+         System.out.println("Is Full, index through node 1: " + full);
+
+      }
+      return full;
    }
    // number of open sites
    public int numberOfOpenSites() {
@@ -82,21 +100,35 @@ public class Percolation {
       Percolation myPercolation = new Percolation(9);
       int n = myPercolation.getN();
       System.out.println("Total Sites: " + n + " by " + n + " = " + n*n ); 
+      System.out.println("Opening 2,1");
+      myPercolation.open(2,1);
+      System.out.println("Opening 2,2");
+      myPercolation.open(2,2);
+      System.out.println("Opening 3,2");
+      myPercolation.open(3,2);
+      System.out.println("Opening 3,3");
+      myPercolation.open(3,3);
       System.out.println("Opening 3,4");
       myPercolation.open(3,4);
       System.out.println("Opening 3,5");
       myPercolation.open(3,5);
-      System.out.println("Opening 7,7");
-      myPercolation.open(7,7);
+      System.out.println("Opening 1,1");
+      myPercolation.open(1,1);
       System.out.println("Is 3,4 open?: " + myPercolation.isOpen(3,4));
       System.out.println("Is 4,4 open?: " + myPercolation.isOpen(4,4));
-      int index1 = myPercolation.xyTo1D(3,4);
+      int index1 = myPercolation.xyTo1D(1,4);
       int index2 = myPercolation.xyTo1D(3,6);
       int index3 = myPercolation.xyTo1D(3,5);
       System.out.println("3,4, and 3,6 connected?: " + myPercolation.quickUnionUF.connected(index1, index2));
-      System.out.println("3,4, and 3,5 connected?: " + myPercolation.quickUnionUF.connected(index1, index3));
+      System.out.println("1,4, and 3,5 connected?: " + myPercolation.quickUnionUF.connected(index1, index3));
       System.out.println("Number of open sites: " + myPercolation.numberOfOpenSites());
-      
+      System.out.println("3,4 is full? " + myPercolation.isFull(3,4));
+      try {
+         Percolation temp = new Percolation(-1);
+      } catch (java.lang.IllegalArgumentException iae) {
+         System.err.println("error: " + iae.getMessage());
+      }
+
    }
    public int xyTo1D(int row, int col) {
       if (xyValid(row, col)) {
