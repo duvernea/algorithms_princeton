6fc5d9baa7724e55679291a791428eaea16c140f
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-12-27 20:58:10 -0600

    hw1 - feat: Implement open and isOpen in Percolation

--- hw1_percolation/Percolation.java
+++ hw1_percolation/Percolation.java
@@ -11,25 +11,45 @@ public class Percolation {
    // create n-by-n grid, with all sites blocked 
    public Percolation(int n) {
       N = n;
-      sites = new int[N*N];
-      for (int i=0; i<N*N-1; i++) {
-         sites[i] = i;
-      }
+      sites = new int[N*N];  // values initialized to 0 in java, none open
+      // for (int i=0; i<N*N-1; i++) {
+      //    sites[i] = i;
+      // }
       quickUnionUF = new WeightedQuickUnionUF(N*N);
    }  
    public int getN() {
-      return N*N;
+      return N;
    }
    public int getValue(int idx) {
       return sites[idx];
    }
    // open site (row, col) if it is not open already
    public void open(int row, int col) {
-
+      if (xyValid(row, col)) {
+         int index = xyTo1D(row, col);
+         sites[index] = 1;
+         //right
+         if (col != N) {
+            quickUnionUF.union(index, index+1);
+         }
+         // left
+         if (col != 1) {
+            quickUnionUF.union(index, index-1);
+         }
+         // up
+         if (row != 1) {
+            quickUnionUF.union(index, index-N);
+         }
+         // down
+         if (row != N) {
+            quickUnionUF.union(index, index+N);
+         }
+      }
    } 
    // is site (row, col) open? 
    public boolean isOpen(int row, int col) {
-      return true;
+      int index = xyTo1D(row, col);
+      return (sites[index] == 1);
    } 
    // is site (row, col) full?
    public boolean isFull(int row, int col) {
@@ -47,15 +67,11 @@ public class Percolation {
    public static void main(String[] args) {
       Percolation myPercolation = new Percolation(9);
       int n = myPercolation.getN();
-      System.out.println("Total Sites: " + n); 
-      for (int i=0; i<n-1; i++) {
-         System.out.println("Site " + i + " Value: " + myPercolation.getValue(i) );
-      }
-      System.out.println("xyTo1D(3, 6): " + myPercolation.xyTo1D(3,6));
-      System.out.println("xyValid(10,9): " + myPercolation.xyValid(10,9));
-      System.out.println("xyValid(0,1): " + myPercolation.xyValid(0,1));
-      System.out.println("xyValid(2,1): " + myPercolation.xyValid(2,1));
-
+      System.out.println("Total Sites: " + n + " by " + n + " = " + n*n ); 
+      System.out.println("Opening 3,4");
+      myPercolation.open(3,4);
+      System.out.println("Is 3,4 open?: " + myPercolation.isOpen(3,4));
+      System.out.println("Is 4,4 open?: " + myPercolation.isOpen(4,4));
    }
    public int xyTo1D(int row, int col) {
       if (xyValid(row, col)) {
