2825952afea99d4f5eda85e4c18e8e175a2db3d2
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-01-03 14:34:04 -0600

    hw2 - feat: Start implementation of Deque using linked lists

--- hw2_queues/Deque.java
+++ hw2_queues/Deque.java
@@ -4,7 +4,7 @@ public class Deque<Item> implements Iterable<Item> {
 
 	private Node first;
 	private Node last;
-	private int N;
+	private int N = 0;
 
 	private class Node {
 		Item item;
@@ -21,17 +21,33 @@ public class Deque<Item> implements Iterable<Item> {
 
    	// return the number of items on the deque
    	public int size() {
-   		return 0;
+   		return N;
    	}           
 
    	// add the item to the front
 	public void addFirst(Item item) {
 
+		Node newFirst = new Node();
+		newFirst.item = item;
+		newFirst.next = first;
+
+		first = newFirst;
+		N++;
+		if (N == 1) {
+			last = first;
+		}
+
 	}
 
 	// add the item to the end        
 	public void addLast(Item item) {
+		Node oldLast = last;
 
+		Node newLast = new Node();
+		oldLast.next = newLast;
+		newLast.item = item;
+
+		last = newLast;
 	}
 
 	// remove and return the item from the front
@@ -41,7 +57,7 @@ public class Deque<Item> implements Iterable<Item> {
 
 	// remove and return the item from the end
 	public Item removeLast() {
-		return first.item;
+		return last.item;
 	} 
 
 	// return an iterator over items in order from front to end              
@@ -66,6 +82,18 @@ public class Deque<Item> implements Iterable<Item> {
 
 	// unit testing       
 	public static void main(String[] args)  {
+
+		Deque<String> myDeque = new Deque<String>();
+
+		myDeque.addFirst("testing 1");
+		myDeque.addFirst("testing 2");
+		myDeque.addFirst("Last item added to beginning");
+		myDeque.addLast("ITEM ADDED AT LAST");
+		System.out.println("Deque size: " + myDeque.size());
+
+		// Item not actually removed for now
+		System.out.println("First item: " + myDeque.removeFirst());
+		System.out.println("Last item: " + myDeque.removeLast());
 
 	} 
 }
