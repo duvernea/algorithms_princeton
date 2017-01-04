5ded657748373e12b549fc8fb6e300023b88d6a5
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-01-04 10:40:49 -0600

    hw2 - feat: Add RandomizedQueueIterator private class

--- hw2_queues/RandomizedQueue.java
+++ hw2_queues/RandomizedQueue.java
@@ -2,6 +2,8 @@ import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
+	private Item item;
+
 	// construct an empty randomized queue
    	public RandomizedQueue() {
 
@@ -25,17 +27,38 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	// remove and return a random item
 
    	public Item dequeue() {
+   		return item;
 
    	}
 
    	// return (but do not remove) a random item                 
    	public Item sample() {
+   		return item;
 
 	}
 
 	// return an independent iterator over items in random order            
 	public Iterator<Item> iterator() {
+		return new RandomizedQueueIterator();
+	}
 
+	private class RandomizedQueueIterator implements Iterator<Item> {
+
+		private Item current = item;
+
+		public boolean hasNext() {
+			return current != null;
+		}
+		public void remove() {
+			throw new java.lang.UnsupportedOperationException();
+		}
+
+		public Item next() {
+			if (current == null) {
+				throw new java.util.NoSuchElementException();
+			}
+			return current;
+		}
 	}
 
 	// unit testing       
