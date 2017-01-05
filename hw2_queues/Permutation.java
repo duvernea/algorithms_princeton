33432fd1a47415c59a3a28371e561b9dfdb66760
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-01-05 14:25:13 -0600

    hw2 - feat: Create Permutation class

---
+++ hw2_queues/Permutation.java
@@ -0,0 +1,13 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+
+public class Permutation {
+
+	public static void main(String[] args) {
+
+		int test = StdIn.readInt();
+		StdOut.println("Read integer: " + test);
+
+	}
+}
