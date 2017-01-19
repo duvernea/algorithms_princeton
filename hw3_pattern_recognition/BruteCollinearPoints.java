7b57e48e2525b11825591260dbd33d9b31777072
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-01-19 15:31:11 -0600

    hw3 - feat: Create BruteCollinearPoints class

---
+++ hw3_pattern_recognition/BruteCollinearPoints.java
@@ -0,0 +1,23 @@
+public class BruteCollinearPoints {
+
+
+	// finds all line segments containing 4 points
+	public BruteCollinearPoints(Point[] points)  {
+
+   	}
+
+ 	// the number of line segments
+	public int numberOfSegments() {
+		return 0;
+	}
+
+	// the line segments
+	public LineSegment[] segments() {
+		LineSegment[] seg = new LineSegment[1];
+		return seg;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
