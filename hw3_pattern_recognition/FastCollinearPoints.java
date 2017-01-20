10ca1ae381e8b90438af3178012deca4867d4550
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-01-19 22:30:38 -0600

    hw3 - feat: Create FastCollinearPoints class

---
+++ hw3_pattern_recognition/FastCollinearPoints.java
@@ -0,0 +1,32 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.In;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import org.junit.runner.JUnitCore;
+
+public class FastCollinearPoints {
+
+	// finds all line segments containing 4 or more points
+ 	public FastCollinearPoints(Point[] points) {
+
+ 	}
+
+ 	// the number of line segments
+	public int numberOfSegments() {
+		// temp
+		return 0;
+	}
+
+	// the line segments
+	public LineSegment[] segments() {
+		// temp
+		LineSegment[] seg = new LineSegment[1];
+		return seg;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
