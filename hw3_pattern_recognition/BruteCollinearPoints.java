34dad0094350ec48b67f7851a3fff9018066e19b
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-01-19 22:21:20 -0600

    hw3 - feat: Throw exceptions where required and clean up code

--- hw3_pattern_recognition/BruteCollinearPoints.java
+++ hw3_pattern_recognition/BruteCollinearPoints.java
@@ -10,46 +10,38 @@ import org.junit.runner.JUnitCore;
 
 public class BruteCollinearPoints {
 
-	private Point[] mPoints;
-	// private int mNumSegments = 0;
 	private ArrayList<LineSegment> mSegmentsList;
-
-
-	// Write a program BruteCollinearPoints.java that examines 4 points at a time 
-	// and checks whether they all lie on the same line segment, 
-	// returning all such line segments. 
-	// To check whether the 4 points p, q, r, and s are collinear, 
-	// check whether the three slopes between p and q, between p and r, and between p and s are all equal.
-
 
 	// finds all line segments containing 4 points
 	public BruteCollinearPoints(Point[] points)  {
+		if (points == null) {
+			throw new java.lang.NullPointerException();
+		}
 		Point[] collinearPoints = new Point[4];
 		mSegmentsList = new ArrayList<LineSegment>();
-		// mPoints = points;
 		int i, j, k, l;
 		for (i = 0; i < points.length - 3; i++ ) {
 			for (j = i+1; j<points.length - 2; j++) {
 				for (k = j+1; k<points.length - 1; k++) {
 					for (l = k+1; l<points.length; l++) {
-						double slope1 = points[i].slopeTo(points[j]);
-						double slope2 = points[i].slopeTo(points[k]);
-						double slope3 = points[i].slopeTo(points[l]);
-						if (slope1 == slope2 && slope1 == slope3) {
-							collinearPoints[0] = points[i];
-							collinearPoints[1] = points[j];
-							collinearPoints[2] = points[k];
-							collinearPoints[3] = points[l];
-							Arrays.sort(collinearPoints);
-							// StdOut.println("Found 4 collinear points");
-							// StdOut.println("Point 1: " + points[i]);
-							// StdOut.println("Point 2: " + points[j]);
-							// StdOut.println("Point 3: " + points[k]);
-							// StdOut.println("Point 4: " + points[l]);
-							// StdOut.println("First point: " + collinearPoints[0]);
-							// StdOut.println("Last point: " + collinearPoints[3]);
-							LineSegment segment = new LineSegment(collinearPoints[0], collinearPoints[3]);
-							mSegmentsList.add(segment);
+						if (points[i] != null && points[j] != null && points[k] != null & points[l] != null) {
+							double slope1 = points[i].slopeTo(points[j]);
+							double slope2 = points[i].slopeTo(points[k]);
+							double slope3 = points[i].slopeTo(points[l]);
+							if (slope1 == Double.NEGATIVE_INFINITY || slope2 == Double.NEGATIVE_INFINITY || slope3 == Double.NEGATIVE_INFINITY) {
+								throw new java.lang.IllegalArgumentException();
+							}
+							if (slope1 == slope2 && slope1 == slope3) {
+								collinearPoints[0] = points[i];
+								collinearPoints[1] = points[j];
+								collinearPoints[2] = points[k];
+								collinearPoints[3] = points[l];
+								Arrays.sort(collinearPoints);
+								LineSegment segment = new LineSegment(collinearPoints[0], collinearPoints[3]);
+								mSegmentsList.add(segment);
+							}
+						} else {
+							throw new java.lang.NullPointerException();
 						}
 					}
 				}
