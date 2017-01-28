e17e630f3633d0d0da14c8b00b3dceff936a3694
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-01-28 10:49:09 -0600

    hw4 - feat: Implement Comparable in SearchNode
    Currently uses hamming to calculate priority

--- hw4_8puzzle_search/Solver.java
+++ hw4_8puzzle_search/Solver.java
@@ -9,34 +9,51 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
 
-	private class SearchNode {
+	private class SearchNode implements Comparable<SearchNode> {
     	Board board;
     	int numMoves;
     	SearchNode previous;
 
-    	private SearchNode(Board board) {
+    	private SearchNode(Board board, int moves, SearchNode previous) {
     		this.board = board;
-    		numMoves = 0;
-    		previous = null;
+    		numMoves = moves;
+    		this.previous = previous;
     	}
-
+    	public int compareTo(SearchNode that) {
+    		int thisPriority = this.board.hamming() + this.numMoves;
+    		int thatPriority = that.board.hamming() + that.numMoves;
+    		if (thisPriority == thatPriority) {
+    			return 0;
+    		} else if (thisPriority < thatPriority) {
+    			return -1;
+    		} else {
+    			return +1;
+    		}
+    	}
 	}
 
 	// find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
 
+    	int moves = 0;
+
     	MinPQ<SearchNode> pq = new MinPQ<SearchNode>();
-    	SearchNode initialNode = new SearchNode(initial);
+    	SearchNode initialNode = new SearchNode(initial, 0, null);
     	pq.insert(initialNode);
     	StdOut.println("Size of priority queue initial: " + pq.size());
 
     	SearchNode min = pq.delMin();
     	Iterable<Board> neighbs1 = min.board.neighbors();
         Iterator<Board> iterator = neighbs1.iterator();
+        moves++;
         while (iterator.hasNext()) {
             Board board = iterator.next();
+            SearchNode node = new SearchNode(board, moves, min);
+            pq.insert(node);
             StdOut.println("Board Neighbor Iterator " + board);
         }
+
+    	StdOut.println("Size of priority queue after 1 move: " + pq.size());
 
     }
 
