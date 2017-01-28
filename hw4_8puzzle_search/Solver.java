232081cf98fe877ff3d77cda2b552dfb04d9a803
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-01-27 19:59:01 -0600

    hw4 - feat: Create Solver class

---
+++ hw4_8puzzle_search/Solver.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class Solver {
+
+	// find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+
+    }
+
+	// is the initial board solvable?    
+    public boolean isSolvable() {
+    	return false;
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+    	return 0;
+    }
+
+	// sequence of boards in a shortest solution; null if unsolvable    
+    public Iterable<Board> solution() {
+    	// TEMP, so class compiles
+    	return new Stack<Board>();
+    }
+
+	// solve a slider puzzle (given below)    
+    public static void main(String[] args) {
+
+    }
+}
