da1014128bb24e329b8cbd3dc6a4636d02e6d398
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-01-28 09:26:21 -0600

    hw4 - feat: Create inner class SearchNode, start constructor implementation
    Add read In from main - read in and create initial Board, create Solver

--- hw4_8puzzle_search/Solver.java
+++ hw4_8puzzle_search/Solver.java
@@ -1,9 +1,31 @@
 import java.util.Stack;
 
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Solver {
+
+	private class SearchNode {
+    	Board board;
+    	int numMoves;
+    	SearchNode previous;
+
+    	private SearchNode(Board board) {
+    		this.board = board;
+    		numMoves = 0;
+    		previous = null;
+    	}
+
+	}
 
 	// find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+
+    	MinPQ<SearchNode> queue = new MinPQ<SearchNode>();
+    	SearchNode initialNode = new SearchNode(initial);
+    	queue.insert(initialNode);
+    	StdOut.println("Size of priority queue initial: " + queue.size());
 
     }
 
@@ -26,5 +48,18 @@ public class Solver {
 	// solve a slider puzzle (given below)    
     public static void main(String[] args) {
 
+		// create initial board from file
+		In in = new In(args[0]);
+		int n = in.readInt();
+		int[][] blocks = new int[n][n];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				blocks[i][j] = in.readInt();
+		Board initial = new Board(blocks);
+		StdOut.println("Board read: " );
+		StdOut.println(initial.toString());
+
+		// solve the puzzle
+		Solver solver = new Solver(initial);
     }
 }
