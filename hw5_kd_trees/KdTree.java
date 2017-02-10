054c944ab7ebed33b6f481a82e8068bc5b8b80fb
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-02-09 19:32:11 -0600

    hw5 - feat: Implement set of 'rect' for each node in insert() method

--- hw5_kd_trees/KdTree.java
+++ hw5_kd_trees/KdTree.java
@@ -65,28 +65,46 @@ public class KdTree {
 			root.point = p;
 			root.rect = new RectHV(0, 0, 1, 1);
 		} else {
-			insertNode(root, p, comparatorX);
+			insertNode(root, p, root.rect, comparatorX);
 		}
 	}
-	private Node insertNode(Node node, Point2D p, Comparator<Point2D> pointCompare) {
+	private Node insertNode(Node node, Point2D p, RectHV rect, Comparator<Point2D> pointCompare) {
 		Comparator<Point2D> flippedCompare;
+		boolean compareX;
 		if (pointCompare.equals(Point2D.X_ORDER)) {
+			compareX = true;
 			flippedCompare = Point2D.Y_ORDER;
 		} else {
+			compareX = false;
 			flippedCompare = Point2D.X_ORDER;
 		}
 		if (node == null) {
-			return new Node(p);
+			Node nodeAdded = new Node(p);
+			nodeAdded.rect = rect;
+			StdOut.println("Point " + p + " created with rectHV " + rect);
+			return nodeAdded;
 		}
 		int compare = pointCompare.compare(p, node.point);
-
+		RectHV rectAdded;
 		if (compare == 1) {
-			node.right = insertNode(node.right, p, flippedCompare);
+
+			if (compareX) {
+				rectAdded = new RectHV(node.point.x(), node.rect.ymin(), node.rect.xmax(), node.rect.ymax());
+			} else {
+				rectAdded = new RectHV(node.rect.xmin(), node.point.y(), node.rect.xmax(), node.rect.ymax());
+			}
+			node.right = insertNode(node.right, p, rectAdded, flippedCompare);
+
 		} else if (compare == -1) {
-			node.left = insertNode(node.left, p, flippedCompare);
-		} else {
-			node.point = p;
-		}
+
+			if (compareX) {
+				rectAdded = new RectHV(node.rect.xmin(), node.rect.ymin(), node.point.x() , node.rect.ymax());
+			} else {
+				rectAdded = new RectHV(node.rect.xmin(), node.rect.ymin(), node.rect.xmax(), node.point.y());
+			}
+			node.left = insertNode(node.left, p, rectAdded, flippedCompare);
+
+		} 
 		return node;
 	}
 	// does the set contain point p? 
@@ -138,6 +156,26 @@ public class KdTree {
 	}
 	// unit testing of the methods (optional) 
 	public static void main(String[] args) {
-		JUnitCore.main("TestKdTree");
+		// JUnitCore.main("TestKdTree");
+
+		KdTree kdTree = new KdTree();
+
+		Point2D p1 = new Point2D(.5, .5);
+		kdTree.insert(p1);
+
+		Point2D p2 = new Point2D(.25, .25);
+		kdTree.insert(p2);
+
+		Point2D p3 = new Point2D(.9, .9);
+		kdTree.insert(p3);
+
+		Point2D p4 = new Point2D(.7, .7	);
+		kdTree.insert(p4);
+
+		Point2D p5 = new Point2D(.6, .7	);
+		kdTree.insert(p5);
+
+		Point2D p6 = new Point2D(.6, .95);
+		kdTree.insert(p6);
 	}
 }
