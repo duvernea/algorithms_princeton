60f05ff4db57491b4f20844ff5db5a47296dc1c6
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-02-11 17:43:53 -0600

    hw5 - feat: Implement nearest() in KdTree and cleanup
    Note: At this point, project passes all correctness and memory tests.  It is failing some tests related to timing on insert() and range() for large datasets

--- hw5_kd_trees/KdTree.java
+++ hw5_kd_trees/KdTree.java
@@ -4,6 +4,7 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.Stack;
 import java.util.Comparator;
+import java.lang.Math;
 
 // import org.junit.runner.JUnitCore;
 
@@ -66,14 +67,12 @@ public class KdTree {
 			root.point = p;
 			root.rect = new RectHV(0, 0, 1, 1);
 			size++;
-			// StdOut.println("Point " + p + " created with rectHV " + root.rect);
 
 		} else {
 			insertNode(root, p, root.rect, comparatorX);
 		}
 	}
 	private Node insertNode(Node node, Point2D p, RectHV rect, Comparator<Point2D> pointCompare) {
-		// StdOut.println("insertNode run.  Should be run number of nodes traversed");
 		Comparator<Point2D> flippedCompare;
 		boolean compareX;
 		if (pointCompare.equals(Point2D.X_ORDER)) {
@@ -86,7 +85,6 @@ public class KdTree {
 		if (node == null) {
 			Node nodeAdded = new Node(p);
 			nodeAdded.rect = rect;
-			// StdOut.println("Point " + p + " created with rectHV " + rect);
 			size++;
 			return nodeAdded;
 		}
@@ -94,10 +92,7 @@ public class KdTree {
 
 		RectHV rectAdded;
 		if (p.equals(node.point)) {
-			// point already exists
-			// StdOut.println("equivalent point " + node.point);
 			return node;
-			// System.exit(0);
 		} 
 		if (compare > 0) {
 			if (compareX) {
@@ -108,7 +103,6 @@ public class KdTree {
 					node.rect.xmax(), node.rect.ymax());
 			}
 			node.right = insertNode(node.right, p, rectAdded, flippedCompare);
-			// StdOut.println("node.right...");
 
 		} else if (compare <= 0) {
 			if (compareX) {
@@ -119,10 +113,7 @@ public class KdTree {
 					node.rect.xmax(), node.point.y());
 			}
 			node.left = insertNode(node.left, p, rectAdded, flippedCompare);
-			// StdOut.println("node.left...");
 		} 
-		// StdOut.println("Reached the end of the insertNode method");
-
 		return node;
 	}
 	// does the set contain point p? 
@@ -142,19 +133,10 @@ public class KdTree {
 			return false;
 		}
 		if (p.equals(node.point)) {
-			// point already exists
-			// StdOut.println("equivalent point " + node.point);
 			return true;
-			// System.exit(0);
 		} 
-		Comparator<Point2D> flippedCompare;
-		if (pointCompare.equals(Point2D.X_ORDER)) {
-		 	// StdOut.println("equals X_ORDER compartor");
-		 	flippedCompare = Point2D.Y_ORDER;
-		} else {
-		 	// StdOut.println("equals Y_ORDER compartor");
-		 	flippedCompare = Point2D.X_ORDER;
-		}
+		Comparator<Point2D> flippedCompare = flipComparator(pointCompare);
+
 		int compare = pointCompare.compare(p, node.point);
 		if (compare > 0) {
 			return containsNode(node.right, p, flippedCompare);
@@ -203,7 +185,6 @@ public class KdTree {
 		} else {
 			searchRect(root, rect, comparatorX);
 		}
-		// StdOut.println("pointsInside: " + pointsInside.size());
 		return pointsInside;
 
 	}
@@ -211,7 +192,6 @@ public class KdTree {
 
 		boolean compareX;
 		if (node == null) return;
-		// StdOut.println("searchRect for node with point " + node.point);
 
 		Comparator<Point2D> flippedCompare;
 		if (pointCompare.equals(Point2D.X_ORDER)) {
@@ -221,11 +201,7 @@ public class KdTree {
 		 	compareX = false;
 		 	flippedCompare = Point2D.X_ORDER;
 		}
-		// StdOut.println("rect: " + rect);
-		// StdOut.println("point: " + node.point);
-		// StdOut.println("contains?: " + rect.contains(node.point)+ "\n");
 		if (rect.contains(node.point)) {
-			// StdOut.println("adding point....");
 			pointsInside.push(node.point);
 		}
 		searchRect(node.left, rect, flippedCompare);
@@ -233,9 +209,8 @@ public class KdTree {
 	}
 	// a nearest neighbor in the set to point p; null if the set is empty 
 	public Point2D nearest(Point2D p) {
-		// StdOut.println("Nearest point being calculated...");
 		Comparator<Point2D> comparatorX = Point2D.X_ORDER;
-		if (root.point == null) {
+		if (isEmpty()) {
 			return null;
 		} else {
 			championPoint = root.point;
@@ -244,90 +219,43 @@ public class KdTree {
 			return championPoint;
 		}
 	}
-	private Node searchNearest(Node node, Point2D p, Comparator<Point2D> pointCompare) {
-		// StdOut.println("Point " + p + " searchNearest() recursive at " + node.point);
+	private void searchNearest(Node node, Point2D p, Comparator<Point2D> pointCompare) {
 		if (node == null) {
-			// parent was the closest
-			return node;
+			return;
 		}
-		Comparator<Point2D> flippedCompare;
-		if (pointCompare.equals(Point2D.X_ORDER)) {
-		 	flippedCompare = Point2D.Y_ORDER;
-		} else {
-		 	flippedCompare = Point2D.X_ORDER;
-		}
-		int compare = pointCompare.compare(p, node.point);
 		double distance = p.distanceTo(node.point);
-		// StdOut.println("Distance from " + p + " to " + node.point + " = " + distance);
 		if (distance < championDistance) {
 			championDistance = distance;
 			championPoint = node.point;
-			// StdOut.println("Champion point updated: " + championDistance + " " + championPoint);
+			if (p.equals(node.point)) return;
 		}
-
-		if (compare == 0) {
-			return node;
-		} else if (compare == 1) {
-			if (node.right == null) {
-				// this is the last point here
-				return node;
-			}
-			// true if current champion point is to the right/top of this point node
-			// if so, need to search the right/top side
-			boolean searchRight = (pointCompare.compare(championPoint, node.point) >= 0);
-			// StdOut.println("championPoint: " + championPoint + " node.point: " 
-			// + node.point + " searchright: " + searchRight);
-			if (searchRight) {
-				node = searchNearest(node.right, p, flippedCompare);
-			}
+		Node first, second;
+		if (pointCompare.compare(p, node.point) > 0) {
+			first = node.left;
+			second = node.right;
 		} else {
-			if (node.left == null) {
-				// this is the last point here
-				return node;
-			}
-			// true if current champion point is to the left/bottom of this point node
-			// if so, need to search the right/top side
-			boolean searchLeft = (pointCompare.compare(championPoint, node.point) <= 0);
-			// StdOut.println("championPoint: " + championPoint + " node.point: " 
-			// + node.point + " searchleft: " + searchLeft);
-
-			if (searchLeft) {
-				node = searchNearest(node.left, p, flippedCompare);
-			}
-		} 
-		// StdOut.println("Do we ever reach this point?");
-		return node;
+			first = node.right;
+			second = node.left;
+		}
+		Comparator<Point2D> flippedCompare = flipComparator(pointCompare);
+		if (first != null && first.rect.distanceTo(p) < championDistance) {
+			searchNearest(first, p, flippedCompare);
+		}
+		if (second != null && second.rect.distanceTo(p) < championDistance) {
+			searchNearest(second, p, flippedCompare);
+		}
+	}
+	private Comparator<Point2D> flipComparator(Comparator<Point2D> comparator) {
+		Comparator<Point2D> flippedComparator;
+		if (comparator.equals(Point2D.X_ORDER)) {
+		 	flippedComparator = Point2D.Y_ORDER;
+		} else {
+		 	flippedComparator = Point2D.X_ORDER;
+		}
+		return flippedComparator;
 	}
 	// unit testing of the methods (optional) 
 	public static void main(String[] args) {
 		// JUnitCore.main("TestKdTree");
-	}
-	private void testDraw() {
-		KdTree kdTree = new KdTree();
-
-		Point2D p1 = new Point2D(0.5, 0.5);
-		kdTree.insert(p1);
-
-		Point2D p2 = new Point2D(0.25, 0.25);
-		kdTree.insert(p2);
-
-		Point2D p3 = new Point2D(0.9, 0.9);
-		kdTree.insert(p3);
-
-		Point2D p4 = new Point2D(0.7, 0.7);
-		kdTree.insert(p4);
-
-		Point2D p5 = new Point2D(0.6, 0.7);
-		kdTree.insert(p5);
-
-		Point2D p6 = new Point2D(0.6, 0.95);
-		kdTree.insert(p6);
-
-		kdTree.draw();
-
-		Point2D checkPoint = new Point2D(.61, .7);
-
-		Point2D nearest = kdTree.nearest(checkPoint);
-		// StdOut.println("Nearest point to " + checkPoint + " = " + nearest);
 	}
 }
--- hw5_kd_trees/TestKdTree.java
+++ hw5_kd_trees/TestKdTree.java
@@ -11,218 +11,253 @@ import org.junit.Test;
 
 public class TestKdTree {
 
-	@Test
-	public void testCreateInsertKdTree() {
+	// @Test
+	// public void testCreateInsertKdTree() {
 
+	// 	KdTree kdTree = new KdTree();
+	// 	assertTrue("New KdTree created.  Should be empty", kdTree.isEmpty());
+	// 	assertEquals("New KdTree. Size should be 0", kdTree.size(), 0);
+
+	// 	Point2D p1 = new Point2D(.5, .5);
+	// 	kdTree.insert(p1);
+	// 	assertFalse("Point " + p1 + " inserted.  KdTree should not be empty", kdTree.isEmpty());
+	// 	assertEquals("1 point inserted, size should be 1", kdTree.size(), 1);
+
+	// 	Point2D p2 = new Point2D(.25, .25);
+	// 	kdTree.insert(p2);
+	// 	assertEquals("2 points inserted, size should be 2", kdTree.size(), 2);
+
+	// 	Point2D p3 = new Point2D(.9, .9);
+	// 	kdTree.insert(p3);
+	// 	assertEquals("3 points inserted, size should be 3", kdTree.size(), 3);
+
+	// 	Point2D p4 = new Point2D(.7, .7);
+	// 	kdTree.insert(p4);
+	// 	assertEquals("4 points inserted, size should be 4", kdTree.size(), 4);
+
+	// 	Point2D p5 = new Point2D(.6, .7);
+	// 	kdTree.insert(p5);
+	// 	assertEquals("5 points inserted, size should be 5", kdTree.size(), 5);
+
+	// 	Point2D p6 = new Point2D(.6, .95);
+	// 	kdTree.insert(p6);
+	// 	assertEquals("6 points inserted, size should be 6", kdTree.size(), 6);
+
+	// 	// Insert duplicate point
+	// 	Point2D p7 = new Point2D(.25, .25);
+	// 	kdTree.insert(p7);
+	// 	StdOut.println("Duplicate point tree size = " + kdTree.size());
+	// 	assertEquals("Duplicate point inserted, size should still be 6", kdTree.size(), 6);
+
+	// 	Point2D p8 = new Point2D(0.406360, 0.678100);
+	// 	kdTree.insert(p8);
+	// 	assertEquals("7 points inserted, size should be 7", kdTree.size(), 7);
+
+	// 	Point2D p9 = new Point2D(.000001, 0.000001);
+	// 	kdTree.insert(p9);
+	// 	assertEquals("8 points inserted, size should be 8", kdTree.size(), 8);
+
+	// 	Point2D p10 = new Point2D(.8, .7);
+	// 	kdTree.insert(p10);
+	// 	assertEquals("9 points inserted, size should be 9", kdTree.size(), 9);
+
+	// 	Point2D p11 = new Point2D(.75, .7);
+	// 	kdTree.insert(p11);
+	// 	assertEquals("10 points inserted, size should be 10", kdTree.size(), 10);
+	// }
+	// @Test
+	// public void testCreateInsertKdTree2() {
+
+	// 	KdTree kdTree = new KdTree();
+	// 	assertTrue("New KdTree created.  Should be empty", kdTree.isEmpty());
+	// 	assertEquals("New KdTree. Size should be 0", kdTree.size(), 0);
+
+	// 	Point2D p1 = new Point2D(.5, .5);
+	// 	kdTree.insert(p1);
+	// 	assertFalse("Point " + p1 + " inserted.  KdTree should not be empty", kdTree.isEmpty());
+	// 	assertEquals("1 point inserted, size should be 1", kdTree.size(), 1);
+
+	// 	Point2D p2 = new Point2D(.7, .7);
+	// 	kdTree.insert(p2);
+	// 	assertEquals("2 points inserted, size should be 2", kdTree.size(), 2);
+
+	// 	Point2D p3 = new Point2D(.6, .7);
+	// 	kdTree.insert(p3);
+	// 	assertEquals("3 points inserted, size should be 3", kdTree.size(), 3);
+
+	// 	Point2D p4 = new Point2D(.6, .95);
+	// 	kdTree.insert(p4);
+	// 	assertEquals("4 points inserted, size should be 4", kdTree.size(), 4);
+
+	// 	Point2D p5 = new Point2D(.8, .7);
+	// 	kdTree.insert(p5);
+	// 	assertEquals("5 points inserted, size should be 5", kdTree.size(), 5);
+
+	// 	Point2D p6 = new Point2D(.75, .7);
+	// 	kdTree.insert(p6);
+	// 	assertEquals("6 points inserted, size should be 6", kdTree.size(), 6);
+	// }
+	// @Test
+	// public void testContainsKdTree() {
+
+	// 	KdTree kdTree = new KdTree();
+	// 	assertTrue("New KdTree created.  Should be empty", kdTree.isEmpty());
+	// 	assertEquals("New KdTree. Size should be 0", kdTree.size(), 0);
+
+	// 	Point2D p1 = new Point2D(.5, .5);
+	// 	kdTree.insert(p1);
+	// 	assertFalse("Point " + p1 + " inserted.  KdTree should not be empty", kdTree.isEmpty());
+	// 	assertEquals("1 point inserted, size should be 1", kdTree.size(), 1);
+
+	// 	Point2D p2 = new Point2D(.25, .25);
+	// 	kdTree.insert(p2);
+	// 	assertEquals("2 points inserted, size should be 2", kdTree.size(), 2);
+
+	// 	Point2D p3 = new Point2D(.9, .9);
+	// 	kdTree.insert(p3);
+	// 	assertEquals("3 points inserted, size should be 3", kdTree.size(), 3);
+
+	// 	Point2D p4 = new Point2D(.7, .7	);
+	// 	kdTree.insert(p4);
+	// 	assertEquals("4 points inserted, size should be 4", kdTree.size(), 4);
+
+	// 	Point2D p5 = new Point2D(.6, .7	);
+	// 	kdTree.insert(p5);
+	// 	assertEquals("5 points inserted, size should be 5", kdTree.size(), 5);
+
+	// 	Point2D p6 = new Point2D(.6, .95	);
+	// 	kdTree.insert(p6);
+	// 	assertEquals("6 points inserted, size should be 6", kdTree.size(), 6);
+
+	// 	boolean contain = kdTree.contains(p5);
+	// 	assertTrue("Point " + p5 + " not found with contains()", kdTree.contains(p5));
+	// 	assertTrue("Point " + p1 + " not found with contains()", kdTree.contains(p1));
+
+	// 	Point2D p7 = new Point2D(.61, .61);
+	// 	assertFalse("Point " + p7 + " should not be found with contains()", kdTree.contains(p7));
+	// }
+	// @Test
+	// public void testSearchClosest() {
+	// 	KdTree kdTree = new KdTree();
+
+	// 	Point2D p1 = new Point2D(.5, .5);
+	// 	kdTree.insert(p1);
+
+	// 	Point2D p2 = new Point2D(.25, .25);
+	// 	kdTree.insert(p2);
+
+	// 	Point2D p3 = new Point2D(.9, .9);
+	// 	kdTree.insert(p3);
+
+	// 	Point2D p4 = new Point2D(.7, .7	);
+	// 	kdTree.insert(p4);
+
+	// 	Point2D p5 = new Point2D(.6, .7	);
+	// 	kdTree.insert(p5);
+
+	// 	Point2D p6 = new Point2D(.6, .95);
+	// 	kdTree.insert(p6);
+
+	// 	Point2D checkPoint1 = new Point2D(.1, 0);
+	// 	assertEquals("Nearest point to " + checkPoint1 + " incorrect.", kdTree.nearest(checkPoint1), p2);
+
+	// 	Point2D checkPoint2 = new Point2D(.25, .25);
+	// 	assertEquals("Nearest point to " + checkPoint2 + " incorrect.", kdTree.nearest(checkPoint2), p2);
+
+	// 	Point2D checkPoint3 = new Point2D(.25, .4);
+	// 	assertEquals("Nearest point to " + checkPoint3 + " incorrect.", kdTree.nearest(checkPoint3), p2);
+
+	// 	Point2D checkPoint4 = new Point2D(.49, .5);
+	// 	assertEquals("Nearest point to " + checkPoint4 + " incorrect.", kdTree.nearest(checkPoint4), p1);
+
+	// 	Point2D checkPoint5 = new Point2D(.51, .55);
+	// 	assertEquals("Nearest point to " + checkPoint5 + " incorrect.", kdTree.nearest(checkPoint5), p1);
+
+	// 	Point2D checkPoint6 = new Point2D(.59, .67);
+	// 	assertEquals("Nearest point to " + checkPoint6 + " incorrect = " + kdTree.nearest(checkPoint6), kdTree.nearest(checkPoint6), p5);
+
+	// 	Point2D checkPoint7 = new Point2D(.66, .7);
+	// 	assertEquals("Nearest point to " + checkPoint7 + " incorrect.", kdTree.nearest(checkPoint7), p4);
+
+	// 	Point2D checkPoint8 = new Point2D(.55, .91);;
+	// 	assertEquals("Nearest point to " + checkPoint8 + " incorrect.", kdTree.nearest(checkPoint8), p6);
+
+	// 	Point2D checkPoint9 = new Point2D(.92, .85);;
+	// 	assertEquals("Nearest point to " + checkPoint9 + " incorrect.", kdTree.nearest(checkPoint9), p3);
+
+	// 	Point2D checkPoint10 = new Point2D(.4, .6);;
+	// 	assertEquals("Nearest point to " + checkPoint10 + " incorrect.", kdTree.nearest(checkPoint10), p1);
+	// }
+	@Test
+	public void testSearchClosest2() {
 		KdTree kdTree = new KdTree();
-		assertTrue("New KdTree created.  Should be empty", kdTree.isEmpty());
-		assertEquals("New KdTree. Size should be 0", kdTree.size(), 0);
 
 		Point2D p1 = new Point2D(.5, .5);
 		kdTree.insert(p1);
-		assertFalse("Point " + p1 + " inserted.  KdTree should not be empty", kdTree.isEmpty());
-		assertEquals("1 point inserted, size should be 1", kdTree.size(), 1);
 
-		Point2D p2 = new Point2D(.25, .25);
-		kdTree.insert(p2);
-		assertEquals("2 points inserted, size should be 2", kdTree.size(), 2);
-
-		Point2D p3 = new Point2D(.9, .9);
-		kdTree.insert(p3);
-		assertEquals("3 points inserted, size should be 3", kdTree.size(), 3);
-
-		Point2D p4 = new Point2D(.7, .7);
-		kdTree.insert(p4);
-		assertEquals("4 points inserted, size should be 4", kdTree.size(), 4);
-
-		Point2D p5 = new Point2D(.6, .7);
-		kdTree.insert(p5);
-		assertEquals("5 points inserted, size should be 5", kdTree.size(), 5);
-
-		Point2D p6 = new Point2D(.6, .95);
-		kdTree.insert(p6);
-		assertEquals("6 points inserted, size should be 6", kdTree.size(), 6);
-
-		// Insert duplicate point
-		Point2D p7 = new Point2D(.25, .25);
-		kdTree.insert(p7);
-		StdOut.println("Duplicate point tree size = " + kdTree.size());
-		assertEquals("Duplicate point inserted, size should still be 6", kdTree.size(), 6);
-
-		Point2D p8 = new Point2D(0.406360, 0.678100);
-		kdTree.insert(p8);
-		assertEquals("7 points inserted, size should be 7", kdTree.size(), 7);
-
-		Point2D p9 = new Point2D(.000001, 0.000001);
-		kdTree.insert(p9);
-		assertEquals("8 points inserted, size should be 8", kdTree.size(), 8);
-
-		Point2D p10 = new Point2D(.8, .7);
-		kdTree.insert(p10);
-		assertEquals("9 points inserted, size should be 9", kdTree.size(), 9);
-
-		Point2D p11 = new Point2D(.75, .7);
-		kdTree.insert(p11);
-		assertEquals("10 points inserted, size should be 10", kdTree.size(), 10);
-	}
-	@Test
-	public void testCreateInsertKdTree2() {
-
-		KdTree kdTree = new KdTree();
-		assertTrue("New KdTree created.  Should be empty", kdTree.isEmpty());
-		assertEquals("New KdTree. Size should be 0", kdTree.size(), 0);
-
-		Point2D p1 = new Point2D(.5, .5);
-		kdTree.insert(p1);
-		assertFalse("Point " + p1 + " inserted.  KdTree should not be empty", kdTree.isEmpty());
-		assertEquals("1 point inserted, size should be 1", kdTree.size(), 1);
-
-		Point2D p2 = new Point2D(.7, .7);
-		kdTree.insert(p2);
-		assertEquals("2 points inserted, size should be 2", kdTree.size(), 2);
-
-		Point2D p3 = new Point2D(.6, .7);
-		kdTree.insert(p3);
-		assertEquals("3 points inserted, size should be 3", kdTree.size(), 3);
-
-		Point2D p4 = new Point2D(.6, .95);
-		kdTree.insert(p4);
-		assertEquals("4 points inserted, size should be 4", kdTree.size(), 4);
-
-		Point2D p5 = new Point2D(.8, .7);
-		kdTree.insert(p5);
-		assertEquals("5 points inserted, size should be 5", kdTree.size(), 5);
-
-		Point2D p6 = new Point2D(.75, .7);
-		kdTree.insert(p6);
-		assertEquals("6 points inserted, size should be 6", kdTree.size(), 6);
-	}
-	@Test
-	public void testContainsKdTree() {
-
-		KdTree kdTree = new KdTree();
-		assertTrue("New KdTree created.  Should be empty", kdTree.isEmpty());
-		assertEquals("New KdTree. Size should be 0", kdTree.size(), 0);
-
-		Point2D p1 = new Point2D(.5, .5);
-		kdTree.insert(p1);
-		assertFalse("Point " + p1 + " inserted.  KdTree should not be empty", kdTree.isEmpty());
-		assertEquals("1 point inserted, size should be 1", kdTree.size(), 1);
-
-		Point2D p2 = new Point2D(.25, .25);
-		kdTree.insert(p2);
-		assertEquals("2 points inserted, size should be 2", kdTree.size(), 2);
-
-		Point2D p3 = new Point2D(.9, .9);
-		kdTree.insert(p3);
-		assertEquals("3 points inserted, size should be 3", kdTree.size(), 3);
-
-		Point2D p4 = new Point2D(.7, .7	);
-		kdTree.insert(p4);
-		assertEquals("4 points inserted, size should be 4", kdTree.size(), 4);
-
-		Point2D p5 = new Point2D(.6, .7	);
-		kdTree.insert(p5);
-		assertEquals("5 points inserted, size should be 5", kdTree.size(), 5);
-
-		Point2D p6 = new Point2D(.6, .95	);
-		kdTree.insert(p6);
-		assertEquals("6 points inserted, size should be 6", kdTree.size(), 6);
-
-		boolean contain = kdTree.contains(p5);
-		assertTrue("Point " + p5 + " not found with contains()", kdTree.contains(p5));
-		assertTrue("Point " + p1 + " not found with contains()", kdTree.contains(p1));
-
-		Point2D p7 = new Point2D(.61, .61);
-		assertFalse("Point " + p7 + " should not be found with contains()", kdTree.contains(p7));
-	}
-	@Test
-	public void testSearchClosest() {
-		KdTree kdTree = new KdTree();
-
-		Point2D p1 = new Point2D(.5, .5);
-		kdTree.insert(p1);
-
-		Point2D p2 = new Point2D(.25, .25);
+		Point2D p2 = new Point2D(.25, .7);
 		kdTree.insert(p2);
 
-		Point2D p3 = new Point2D(.9, .9);
+		Point2D p3 = new Point2D(.8, .7);
 		kdTree.insert(p3);
 
-		Point2D p4 = new Point2D(.7, .7	);
-		kdTree.insert(p4);
+		StdOut.println("3 points inserted, size = " + kdTree.size());
 
-		Point2D p5 = new Point2D(.6, .7	);
-		kdTree.insert(p5);
-
-		Point2D p6 = new Point2D(.6, .95);
-		kdTree.insert(p6);
-
-		Point2D checkPoint1 = new Point2D(.1, 0);
+		Point2D checkPoint1 = new Point2D(.28, .7);
 		assertEquals("Nearest point to " + checkPoint1 + " incorrect.", kdTree.nearest(checkPoint1), p2);
 
-		Point2D checkPoint2 = new Point2D(.25, .25);
-		assertEquals("Nearest point to " + checkPoint2 + " incorrect.", kdTree.nearest(checkPoint2), p2);
-
-		Point2D checkPoint3 = new Point2D(.25, .4);
-		assertEquals("Nearest point to " + checkPoint3 + " incorrect.", kdTree.nearest(checkPoint3), p2);
-
-		Point2D checkPoint4 = new Point2D(.49, .5);
-		assertEquals("Nearest point to " + checkPoint4 + " incorrect.", kdTree.nearest(checkPoint4), p1);
-
-		Point2D checkPoint5 = new Point2D(.51, .55);
-		assertEquals("Nearest point to " + checkPoint5 + " incorrect.", kdTree.nearest(checkPoint5), p1);
-
-		Point2D checkPoint6 = new Point2D(.59, .67);
-		assertEquals("Nearest point to " + checkPoint6 + " incorrect.", kdTree.nearest(checkPoint6), p5);
-
-		Point2D checkPoint7 = new Point2D(.66, .7);
-		assertEquals("Nearest point to " + checkPoint7 + " incorrect.", kdTree.nearest(checkPoint7), p4);
-
-		Point2D checkPoint8 = new Point2D(.55, .91);;
-		assertEquals("Nearest point to " + checkPoint8 + " incorrect.", kdTree.nearest(checkPoint8), p6);
-
-		Point2D checkPoint9 = new Point2D(.92, .85);;
-		assertEquals("Nearest point to " + checkPoint9 + " incorrect.", kdTree.nearest(checkPoint9), p3);
-
-		Point2D checkPoint10 = new Point2D(.4, .6);;
-		assertEquals("Nearest point to " + checkPoint10 + " incorrect.", kdTree.nearest(checkPoint10), p1);
 	}
 	@Test
-	public void testSearchRange() {
-		StdOut.println("\n\ntestSearchRange\n\n");
+	public void testVertical() {
 		KdTree kdTree = new KdTree();
-
-		Point2D p1 = new Point2D(.5, .5);
+		Point2D p1 = new Point2D(.3, .9);
 		kdTree.insert(p1);
 
-		Point2D p2 = new Point2D(.25, .25);
+		Point2D p2 = new Point2D(.3, .1);
 		kdTree.insert(p2);
 
-		Point2D p3 = new Point2D(.9, .9);
+		Point2D p3 = new Point2D(.3, .2);
 		kdTree.insert(p3);
 
-		Point2D p4 = new Point2D(.7, .7	);
+		Point2D p4 = new Point2D(.3, .8);
 		kdTree.insert(p4);
 
-		Point2D p5 = new Point2D(.6, .7	);
-		kdTree.insert(p5);
-
-		Point2D p6 = new Point2D(.6, .95);
-		kdTree.insert(p6);
-
-		RectHV rect = new RectHV(.3, .3, .7, .7);
-		// Iterable<Point2D> rectRange = kdTree.range(rect); 
-		// for (Point2D point : kdTree.range(rect)) {
-		// 	StdOut.println("Point " + point + " found inside rect " + rect);
-		// }
-		int numPoints = 0;
-		StdOut.println("Testing for range of points in rectangle...");
-		for (Point2D point : kdTree.range(rect)) {
-			numPoints++;
-			assertFalse("Rect should not contain point (.9, .9)", point.equals(p3));
-			assertFalse("Rect should not contain point (.7, 1)", point.equals(p6));
-		}
-		assertEquals("2 points should be contained in the rectangle", numPoints, 3);
 	}
+	// @Test
+	// public void testSearchRange() {
+	// 	StdOut.println("\n\ntestSearchRange\n\n");
+	// 	KdTree kdTree = new KdTree();
+
+	// 	Point2D p1 = new Point2D(.5, .5);
+	// 	kdTree.insert(p1);
+
+	// 	Point2D p2 = new Point2D(.25, .25);
+	// 	kdTree.insert(p2);
+
+	// 	Point2D p3 = new Point2D(.9, .9);
+	// 	kdTree.insert(p3);
+
+	// 	Point2D p4 = new Point2D(.7, .7	);
+	// 	kdTree.insert(p4);
+
+	// 	Point2D p5 = new Point2D(.6, .7	);
+	// 	kdTree.insert(p5);
+
+	// 	Point2D p6 = new Point2D(.6, .95);
+	// 	kdTree.insert(p6);
+
+	// 	RectHV rect = new RectHV(.3, .3, .7, .7);
+	// 	// Iterable<Point2D> rectRange = kdTree.range(rect); 
+	// 	// for (Point2D point : kdTree.range(rect)) {
+	// 	// 	StdOut.println("Point " + point + " found inside rect " + rect);
+	// 	// }
+	// 	int numPoints = 0;
+	// 	StdOut.println("Testing for range of points in rectangle...");
+	// 	for (Point2D point : kdTree.range(rect)) {
+	// 		numPoints++;
+	// 		assertFalse("Rect should not contain point (.9, .9)", point.equals(p3));
+	// 		assertFalse("Rect should not contain point (.7, 1)", point.equals(p6));
+	// 	}
+	// 	assertEquals("2 points should be contained in the rectangle", numPoints, 3);
+	// }
 }
